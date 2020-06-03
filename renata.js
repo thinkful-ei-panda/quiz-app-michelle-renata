16d61d54d1a66bf4341a74f4f080efb197f471fe
seraphyne <renatafd717@gmail.com>
2020-06-03 18:17:40 -0500

    decided to leaver her code the way we did so far and made a renata.html and renata.js on the side and work with that instead

--- renata.js
+++ renata.js
@@ -63,3 +63,22 @@ const store = {
     questionNumber: 0,
     score: 0
   };
+
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		// code will go here
+	}
+
+	function showResults(questions, quizContainer, resultsContainer){
+		// code will go here
+	}
+
+	// show the questions
+	showQuestions(questions, quizContainer);
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
