a80d2aa8ea1527275e9eb1b444b1a8f389f6c5ab
seraphyne <renatafd717@gmail.com>
2020-06-03 18:12:33 -0500

    Added a renata.js to just do my own code and dont mess up michelles, linked on index.html to renata.js instead of app.js

---
+++ renata.js
@@ -0,0 +1,65 @@
+/* eslint-disable indent */
+/* eslint-disable strict */
+const store = {
+    // 5 or more questions are required
+    questions: [
+      {
+        question: 'Which planet is bigger?',
+        answers: [
+          'Earth',
+          'Mars',
+          'Io',
+          'Saturn',
+          'Venus'
+        ],
+        correctAnswer: 'Saturn'
+      },
+      {
+        question: 'Which of these planets have no moons?',
+        answers: [
+          'Uranus',
+          'Mercury',
+          'Earth',
+          'Sun',
+          'Pluto'
+        ],
+        correctAnswer: 'Mercury'
+      },
+      {
+        question: 'Which of these is the largest object in the asteroid belt?',
+        answers: [
+          'Makemake',
+          'Ceres',
+          '4 Vesta',
+          '3 Juno',
+          'Pallas'
+        ],
+        correctAnswer: 'Ceres'
+      },
+      {
+        question: 'Which galaxy is further away from Earth?',
+        answers: [
+          'Andromeda',
+          'large magellanic cloud',
+          'MACS0647-JD',
+          'Milky Way',
+          'Leo I'
+        ],
+        correctAnswer: 'MACS0647-JD'
+      },
+      {
+        question: 'Which of these can be formed in a supernova?',
+        answers: [
+          'white dwarf',
+          'neutron star',
+          'a galaxy',
+          'alien life',
+          'dark matter'
+        ],
+        correctAnswer: 'neutron star'
+      }
+    ],
+    quizStarted: false,
+    questionNumber: 0,
+    score: 0
+  };
